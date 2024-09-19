6da4729cebff992f10149dfe263a23dda69d4fbf
José Afonso da Silva Pereira <j2007afonso@gmail.com>
2024-09-19 10:22:19 -0300

    Meu banco de dados

---
+++ src/routes/productRoutes.js
@@ -0,0 +1,8 @@
+import {getProducts} from'../controllers/productController.js';
+import {Router} from "express";
+
+const router = Router();
+
+router.get('/products', getProducts);
+
+export default router;
