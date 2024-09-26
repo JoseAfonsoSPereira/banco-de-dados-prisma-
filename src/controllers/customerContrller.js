20e4163d06b63b2bdebc630faab9bce71ffa12f8
José Afonso da Silva Pereira <j2007afonso@gmail.com>
2024-09-26 13:32:19 -0300

    completando o banco de dados dos clientes

--- src/controllers/customerContrller.js
+++ src/controllers/customerContrller.js
@@ -9,3 +9,48 @@ export const getCustomers = async (req, res) => {
         res.status(500).json({error: "Erro ao buscar",})
     }
 }
+
+export const createCustomerController = async (req, res) =>{
+    try{
+        const newCustomer = await createCustomer(req.body);
+        res.status(201).json(newCustomer)
+    }
+    catch(error){
+        res.status(500).json({error: "Erro ao cadastrar cliente",})
+    }
+};
+
+export const getCustomer = async (req, res) =>{
+    try{
+        const customer = await getCustomerById(req.params.id);
+        if(customer){
+             res.json(customer)
+        } else {
+            res.status(404).json({error: "Cliente não encontrado"})
+        }
+        
+    } 
+    catch(error){
+        res.status(500).json({error: "Erro ao buscar cliente",})
+    }
+};
+
+export const updateCustomerController = async(req, res)=>{
+    try{
+        const customer = await updateCustomer(req.params.id, req.body);
+        res.json(customer);
+    }
+    catch(error){
+        res.status(500).json({error: "Erro ao atualizar cliente",})
+    }
+}
+
+export const deleteCustomerController = async(req, res)=>{
+    try{
+        await deleteCustomer(req.params.id)
+        res.status(204).send();
+    }
+    catch(error){
+        res.status(500).json({error: "Erro ao deletar cliente",})
+    }
+}
--- src/routes/customerRoute.js
+++ src/routes/customerRoute.js
@@ -1,8 +1,12 @@
-import {getCustomers} from'../controllers/customerContrller.js';
+import {getCustomers, createCustomerController,  getCustomer, updateCustomerController, deleteCustomerController} from'../controllers/customerContrller.js';
 import {Router} from "express";
 
 const router = Router();
 
 router.get('/customers', getCustomers);
+router.post("/products", createCustomerController)
+router.get("/products/:id", getCustomer)
+router.patch("/products/:id", updateCustomerController)
+router.delete("/products/:id",deleteCustomerController)
 
 export default router;
